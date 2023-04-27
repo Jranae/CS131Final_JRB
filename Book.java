ce5ad6926a12a804055fced1f26d9adc283b9f30
Jranae <112411872+Jranae@users.noreply.github.com>
2023-04-27 15:25:09 -0400

    Add files via upload

--- Book.java
+++ Book.java
@@ -1,26 +1,50 @@
+import java.util.Arrays;
+import java.util.Scanner;
 /*This Class will be on a Bookstore that is having a 40% off sale
-* user will go through and pick out books of interest and go to check out
+*Program uses Scanner class
 *This class holds data such as title, author, pages, price, and discounted price
-* @author Jaleah Beason
+*
+  @author Jaleah Beason
+  @version 1.0
 */
 public class Book implements Discounts {
-	 	private String _title;
+		private String _title;
 	    private String _author;
 	    private int _pages;
 	    private double _price;
 	    public int numBooks = 1;
-	    public  Book(){
-	        for (int i = 0; i > 6; i++)
+    	public int book =0 ;
+    	public int quantity;
+		private double subTotal;
+		private String [] nameBook;
+	    
+		public  Book(){
+	        for (int i = 0; i > 4; i++)
 	            numBooks++;
 
 	    }
-
-	    public Book(String title, String author, int pages, double price)
+	    public Book( int quantity, double subTotal, String [] nameBook)
 	    {  
-	        this._title = title;
-	        this._author = author;
-	        this._pages = pages;
-	        this._price = price;
+	        this.quantity = quantity;
+			this.subTotal = subTotal;
+			nameBook = new String[4];
+			//System.out.println("created name book");
+			
+	    }
+	 
+	    //implementing a string builder for receipt
+	    public String recieptStructure(){
+			
+			StringBuilder sb = new StringBuilder("==================================\n");
+			sb.append("Book: _title:" + _title +"\n");
+			sb.append(" _author:" + _author + "\n");
+			sb.append(" _pages:" + _pages +"\n");
+			sb.append(" _price:" + _price +"\n");
+			sb.append(" numBooks=:" + numBooks + "\n");
+			sb.append(" reducedPrice:" + reduceBooks() + "\n");
+			sb.append("==================================\n");
+			
+			return sb.toString();
 	    }
 
 	    public String getTitle()
@@ -71,18 +95,82 @@ public class Book implements Discounts {
 	        this._price = price;
 
 	    }
-	    public double reduceBooks() 
+
+		public double getSubTotal() {
+			return subTotal;
+		}
+
+		public void setSubTotal(double subTotal) {
+			this.subTotal = subTotal;
+		}
+
+		public String[] getNameBook() {
+			return nameBook;
+		}
+
+		public double reduceBooks() 
 	    {
-	    double per = .4;
-	    double cost = getPrice();
-	    double reducedPrice = (cost)-(per * cost);
+			double per = .4;
+			double cost = getPrice();
+			double reducedPrice = (cost)-(per * cost);
 
 	     return reducedPrice;
 	    }
+	    public void orderBook() {
+	    	Scanner scan = new Scanner(System.in);
+	    	Scanner input;
+	    	input = new Scanner(System.in);
+			do {
+	            System.out.println("Please Enter A Book Number Between 1-4 or Press 0 to Exit");
+	            book = input.nextInt();
+
+	            if (book == 0)
+	                break;
+
+	            System.out.println("Please Enter Quantity");
+	            int quantity = scan.nextInt();
+
+	            switch (book) {
+	                case 1:
+	                    double bookPrice1 = 149.99;
+	                    subTotal += (149.99 * quantity);
+	                    System.out.println("It Starts with Us");
+	                    break;
+	                case 2:
+	                	double bookPrice2 = 10.99;
+	                    subTotal += (10.99 * quantity);
+	                    nameBook[1] = "It Ends with Us";
+	                    break;
+	                case 3:
+	                	double bookPrice3 = 8.99;
+	                    subTotal += (8.99 * quantity);
+	                    nameBook[2] = "A Dance with Dragons";
+	                    break;
+	                case 4:
+	                	double bookPrice4 = 21.42;
+	                    subTotal += (21.42 * quantity);
+	                    nameBook[3] = "The Winds of Winter";
+	                    break;
+
+	                default:
+	                    System.out.println("Please enter a valid product No.");
+	            }
+			}
+		while (book != 0);
+		        for (int i = 1; i < 5; i++) {
+		        	System.out.println( "Product: " + nameBook[i]
+		        		    + " Quantity: " + quantity++);
+		        }
+			}
+	    
+     
+
 	    @Override
 		public String toString() {
-			return "Book [_title:" + _title + ", _author:" + _author + ", _pages:" + _pages + ", _price:" + _price
-					+ ", numBooks=:" + numBooks + ", reducedPrice:" + reduceBooks() + "]\n";
+			return "Book [_title=" + _title + ", _author=" + _author + ", _pages=" + _pages + ", _price=" + _price
+					+ ", numBooks=" + numBooks + ", book=" + book + ", quantityOrdered="
+					+ ", subTotal=" + subTotal + ", nameBook="
+					+ Arrays.toString(nameBook) + ", recieptStructure()=" + recieptStructure() + "]";
 		}
 	    
 }
--- MyBook.java
+++ MyBook.java
@@ -1,39 +1,49 @@
 import java.util.*;
-/*creates an array that is capable of hold four Book objects
- * uses the parameterized constructor to specify the data 
- * in the first four elements of the array
- * calls reduceBooks method to display the discount on the four books
- * @author Jaleah Beason
+/*  creates an array that is capable of hold four Book objects
+ *  uses the parameterized constructor to specify the data 
+ *  in the first four elements of the array
+ *  calls reduceBooks method to display the discount on the four books
+ *  @author Jaleah Beason
+ *  @version 1.0
  */
 public class MyBook extends Book implements Discounts{
-
+//book 1 89.994
+//book 2 6.
+//book 3 8.99
+//book 4 21.42
 
  public static void main(String[] args){
 
-     Book[] books = new Book[6];
-
-     Book book1 = new Book("It Starts with Us", "Collen Hoover", 1320, 149.99);
-     Book book2 = new Book("It Ends with Us", "Colleen Hoover", 350, 10.99);
-     Book book3 = new Book("A Dance with Dragons", "Georger R. R. Martin", 180, 8.99);
-     Book book4 = new Book("The Winds of Winter", "George R. R. Martin", 320, 21.42);
+     Book[] books = new Book[4];
      
-
-
+     Book book1 = new Book(1 , 89.99 ,"" [1]);
+     book1.orderBook();
+     System.out.println(book1.recieptStructure());
+     //Book book2 = new Book(int [] quantityOrdered, double subTotal, String [] nameBook);
+     //book2.orderBook();
+     //System.out.println(book2.recieptStructure());
+     //Book book3 = new Book( int [] quantityOrdered, double subTotal, String [] nameBook);
+    // book3.orderBook();
+    // System.out.println(book3.recieptStructure());
+    // Book book4 = new Book(int [] quantityOrdered, double subTotal, String [] nameBook);
+     //book4.orderBook();
+     
+     
+     System.out.println(book1.recieptStructure());
+     //System.out.println(book2.recieptStructure());
+     //System.out.println(book3.recieptStructure());
+     //System.out.println(book4.recieptStructure());
+    
      books[0] = book1;
-     books[1] = book2;
-     books[2] = book3;
-     books[3] = book4;
-     
-
-     for (Book u : books)
-         System.out.print(u);
+     //books[1] = book2;
+     //books[2] = book3;
+     //books[3] = book4;
      
      book1.reduceBooks();
-     book2.reduceBooks();
-     book3.reduceBooks();
-     book4.reduceBooks();
-    
- 
+     //book2.reduceBooks();
+     //book3.reduceBooks();
+     //book4.reduceBooks();
+     
 		}
 
  }
